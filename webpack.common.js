116d3ae62b99b85c564f2b604d340347794c7c8e
Sayed Alwedaie <5216479+sydalwedaie@users.noreply.github.com>
2026-03-28 23:36:57 +0530

    split config for dev and prod

---
+++ webpack.common.js
@@ -0,0 +1,46 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+
+export default {
+  entry: {
+    index: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "oDine",
+      template: "./src/template.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public", to: "" }],
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(import.meta.dirname, "dist"),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
